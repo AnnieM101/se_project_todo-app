a0e0c2aac401ac9a64cb72bb19c6555c2b905c71
AnnieM101 <annacarolinekatherine@gmail.com>
2025-04-15 02:53:25 -0400

    Initial JavaScript refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,4 @@
-const initialTodos = [
-  {
-    id: "7cec7373-681b-49d9-b065-021d61a69d03",
-    name: "Read the sprint's theory",
-    completed: true,
-    date: new Date(),
-  },
-  {
-    id: "a7bfd5ef-37cc-4fa6-89f2-cac098a8aeba",
-    name: "Read project instructions",
-    completed: false,
-    date: new Date(),
-  },
-  {
-    id: "aa486839-63ab-437f-b8a2-29ab217dff4f",
-    name: "Complete project",
-    completed: false,
-    date: new Date(),
-  },
-];
-
-const validationConfig = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  errorClass: "popup__error_visible",
-  inputErrorClass: "popup__input_type_error",
-  inactiveButtonClass: "button_disabled",
-};
+import { initialTodos, validationConfig } from "../utils/constants.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -54,8 +26,14 @@ const generateTodo = (data) => {
   const todoDate = todoElement.querySelector(".todo__date");
   const todoDeleteBtn = todoElement.querySelector(".todo__delete-btn");
 
-  todoNameEl.textContent = data.name;
-  todoCheckboxEl.checked = data.completed;
+  class Todo {
+    constructor(data, selector) {
+      todoNameEl.textContent = data.name;
+      todoCheckboxEl.checked = data.completed;
+    }
+    _setEventListeners() {}
+    getView() {}
+  }
 
   // Apply id and for attributes.
   // The id will initially be undefined for new todos.
